0710c4b668d51db6e18447dce7db1021b94557af
norfadilamohdyunus <norfadilamohdyunus@gmail.com>
2025-12-01 09:49:31 +0000

    Console app with EF Core + SQLite

---
+++ My1stProject/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace My1stProject
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=myconsoleapp.db");
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ My1stProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace My1stProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AppDbContext())
+            {
+                db.Database.EnsureCreated(); // Create db if not exist
+
+                // Add user
+                db.Users.Add(new User { Name = "Fadila" });
+                db.SaveChanges();
+
+                // Read users
+                var users = db.Users.ToList();
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"Id: {user.Id}, Name: {user.Name}");
+                }
+            }
+        }
+    }
+}
